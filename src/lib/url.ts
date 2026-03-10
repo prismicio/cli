bab4ae0602999e5d777d48576088a1da7b13b6cd
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2026-03-09 19:04:51 -1000

    chore: remove some dead code

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,27 +1,8 @@
 import { getHost } from "./auth";
 
-export async function getRepoUrl(repo: string): Promise<URL> {
-	const host = await getHost();
-	host.hostname = `${repo}.${host.hostname}`;
-	return appendTrailingSlash(host);
-}
-
-export async function getInternalApiUrl(): Promise<URL> {
-	const host = await getHost();
-	host.hostname = `api.internal.${host.hostname}`;
-	return appendTrailingSlash(host);
-}
-
 export async function getUserServiceUrl(): Promise<URL> {
 	const host = await getHost();
-	host.hostname = `user-service.${host.hostname}`;
-	return appendTrailingSlash(host);
-}
-
-export async function getAuthUrl(): Promise<URL> {
-	const host = await getHost();
-	host.hostname = `auth.${host.hostname}`;
-	return appendTrailingSlash(host);
+	return new URL(`https://user-service.${host}/`);
 }
 
 export function appendTrailingSlash(url: string | URL): URL {
